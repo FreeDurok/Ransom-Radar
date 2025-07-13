08557cdd027cb1bde9c9aa684bb8b178404a3352
Durok <durok1@protonmail.com>
2025-07-13 16:47:15 +0200

    fix config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POLL_INTERVAL = 10 # seconds
+POLL_INTERVAL = 150 # seconds
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 LOG_FILE_PATH = os.path.expanduser("~/.ransom-radar/ransom_radar.log")
