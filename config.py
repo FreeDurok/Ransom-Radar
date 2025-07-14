a8afb2cd02db9bdd78ad29782bd56c52b267d0fe
Durok <durok1@protonmail.com>
2025-07-14 12:17:48 +0200

    config fix

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 # AI Configuration
 AI_ENABLED = True
-AI_MODEL = "moonshotai/kimi-k2-instruct"
-# AI_MODEL="gpt-4",
-# AI_MODEL="deepseek/deepseek-r1-0528",
 
 # OpenAI Configuration
 # API_KEY = os.getenv("OPENAI_API_KEY")
 # API_URL = "https://api.openai.com/v1/responses"
+# AI_MODEL="gpt-4",
 
 # Hugging Face Token for Novita Inference API
 API_KEY = os.getenv("HF_API_KEY", None)
 API_URL = "https://router.huggingface.co/novita/v3/openai"
+AI_MODEL = "moonshotai/kimi-k2-instruct"
+# AI_MODEL="deepseek/deepseek-r1-0528",
 
 # Proxy Configuration
 PROXY_URL = os.getenv("PROXY_URL", None)
